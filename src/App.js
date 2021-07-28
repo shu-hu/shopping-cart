3eebdb65668a4f01bd0bb10e2e31fd4de3d1d348
teradaian <teradaian@gmail.com>
2021-07-28 12:34:58 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
+import AllTheThings from "./Components/AllTheThings";
+import MyShoppingCart from "./Components/MyShoppingCart";
+import productsArr from "./products";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Big Time Shopping</h1>
+      <AllTheThings />
+      <MyShoppingCart />
     </div>
   );
 }
-
-export default App;
